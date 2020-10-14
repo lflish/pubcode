0215cf0f5f32c9cb887576183aed10774c15d955
小宇子 <lflish@192.168.124.11>
2020-10-14 21:22:41 +0800

    整理readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #　公开项目
+
+c/lib               封装通用基础库
+c/host-security     主机监控程序
---
+++ c/lib/librunlog/runlog.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#define MSG_DATA_LEN	4096
+
+#define LOG_ERROR 	"error", __FILE__, __LINE__, __FUNCTION__
+#define LOG_DEBUG 	"debug", __FILE__, __LINE__, __FUNCTION__
+
+
+int runlog_open();
+int debuginfo(const char *type, char * file, int line, const char * fun, char *format, ...);
+void runlog_close();
