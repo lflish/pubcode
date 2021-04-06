46553139cc53203348fedfb44ac83907d9e38f4d
lflish_ubuntu20 <hxy.gold@gmail.com>
2021-04-06 04:10:31 -0700

    基本数据

---
+++ golang/study/ch2/popcount.go
@@ -0,0 +1,27 @@
+package ch2
+
+import "fmt"
+
+var pc [256]byte
+
+/* 创建bitmap */
+func init(){
+	for i := range pc{
+	//for i, _:= range pc{ /* 也可以用这种方式 */
+			pc[i] = pc[i/2] + byte(i & 1)
+			fmt.Printf("%d %d %d %d\n", i, pc[i], pc[i/2], byte(i & 1))
+	}
+	fmt.Printf("\n")
+}
+
+func PopCount(x uint64) int {
+	fmt.Printf("2\n")
+	return int(pc[byte( x >> (0 * 8))] +
+		pc[byte(x >> (1 * 8))] +
+		pc[byte(x >> (2 * 8))] +
+		pc[byte(x >> (3 * 8))] +
+		pc[byte(x >> (4 * 8))] +
+		pc[byte(x >> (5 * 8))] +
+		pc[byte(x >> (6 * 8))] +
+		pc[byte(x >> (7 * 8))])
+}
---
+++ golang/study/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"./ch2"
+	"./ch3"
+	"fmt"
+)
+
+
+func test(){
+	var i uint64
+	for i = 0; i < 0x0f; i++ {
+		fmt.Printf("PopCount:%x=%d\n", i, ch2.PopCount(i))
+	}
+}
+
+func main(){
+	//test()
+	ch3.Test1()
+}
